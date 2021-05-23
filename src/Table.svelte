62f760624d2e77b132ffe6c3ce0f076624d3d477
cch099 <50557026+cch099@users.noreply.github.com>
2021-05-23 11:11:19 -0600

    Finished with remove, including stopprogation

--- src/Table.svelte
+++ src/Table.svelte
@@ -1,59 +1,68 @@
 <script>
-    import { createEventDispatcher } from "svelte";
-    import materialStore from "./material-store.js";
-  
-    const dispatch = createEventDispatcher();
-    let materials = [];
-  
-    materialStore.subscribe(items => {
-      materials = items;
-    });
-  
-    $: total = materials.reduce((prev, next) => {
-      prev += next.price;
-      return prev;
-    }, 0);
-  
-    function edit(id, name, price) {
-      dispatch("edit", { id, name, price });
-    }
-  
-    const formatter = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD"
-    });
-  </script>
-  
-  <style>
-    table {
-      width: 100%;
-    }
-    tr {
-      cursor: pointer;
-    }
-  </style>
-  
-  <table class="primary">
-    <thead>
-      <tr>
-        <th>Material</th>
-        <th>Price</th>
-        <th />
+  import { createEventDispatcher } from "svelte";
+  import materialStore from "./material-store.js";
+
+  const dispatch = createEventDispatcher();
+  let materials = [];
+
+  materialStore.subscribe(items => {
+    materials = items;
+  });
+
+  $: total = materials.reduce((prev, next) => {
+    prev += next.price;
+    return prev;
+  }, 0);
+
+  function edit(id, name, price) {
+    dispatch("edit", { id, name, price });
+  }
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
+  function remove(id) {
+    materialStore.remove(id);
+  }
+</script>
+
+<style>
+  table {
+    width: 100%;
+  }
+  tr {
+    cursor: pointer;
+  }
+  tr:last-of-type {
+    cursor: inherit;
+  }
+</style>
+
+<table class="primary">
+  <thead>
+    <tr>
+      <th>Material</th>
+      <th>Price</th>
+      <th />
+    </tr>
+  </thead>
+  <tbody>
+    {#each materials as material (material.id)}
+      <tr on:click={edit(material.id, material.name, material.price)}>
+        <td>{material.name}</td>
+        <td>{formatter.format(material.price)}</td>
+        <td>
+          <i
+            on:click|stopPropagation={remove(material.id)}
+            class="far fa-trash-alt" />
+        </td>
       </tr>
-    </thead>
-    <tbody>
-      {#each materials as material (material.id)}
-        <tr on:click={edit(material.id, material.name, material.price)}>
-          <td>{material.name}</td>
-          <td>{formatter.format(material.price)}</td>
-          <td>
-            <i class="far fa-trash-alt" />
-          </td>
-        </tr>
-      {/each}
-      <tr>
-        <td>Total</td>
-        <td colspan="2">{formatter.format(total)}</td>
-      </tr>
-    </tbody>
-  </table>
+    {/each}
+    <tr>
+      <td>Total</td>
+      <td colspan="2">{formatter.format(total)}</td>
+    </tr>
+  </tbody>
+</table>
